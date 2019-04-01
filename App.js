80438a79a07b8d47cd7abf01b333f86b57fbed15
Thomas Finney <tomfinney25@gmail.com>
2019-04-01 16:04:03 +0100

    blue text

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Text, View, TouchableOpacity } from "react-native";
+import styles from "./styles.scss";
 
 export default function App() {
   const [pressed, setPressed] = useState(0);
@@ -36,6 +37,9 @@ export default function App() {
         <Text style={{ color: "white" }}>
           Button has been pressed:{" "}
           <Text style={{ fontWeight: "bold" }}>{pressed}</Text> times
+          {"\n"}
+          {"\n"}
+          <Text className={styles.blue}>Blue TEXT!</Text>
         </Text>
       </View>
       <View style={{ flex: 3, backgroundColor: "steelblue" }} />
